bed49399de42e9a0dedbefecb0dd7ca31146338e
Nima H <nimahz@Nimas-MacBook-Air.local>
2023-02-01 22:26:26 -0500

    initial commit

---
+++ src/components/PlayerList.js
@@ -0,0 +1,17 @@
+// PlayerList.js
+
+import React from "react"; //optional
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
+import Player from "./Player";
+
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player />
+    </section>
+  );
+}
+
+export default PlayerList;
